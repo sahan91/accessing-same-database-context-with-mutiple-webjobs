2757c7235cbee61183d6afd82edb79a5744bdf6a
Sahan Serasinghe <sahan.serasinghe@gmail.com>
2020-07-16 00:21:37 +0930

    Add Functions code

---
+++ WebJobsDemo/Functions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace WebJobsDemo
+{
+    public class Functions
+    {
+        public static void ProcessQueueMessage([QueueTrigger("queue")] string message, ILogger logger)
+        {
+            logger.LogInformation(message);
+        }
+    }
+}
