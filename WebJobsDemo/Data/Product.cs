e5a53b918c051ee7adf73bf507b29ff66d7d251b
Sahan Serasinghe <sahan.serasinghe@gmail.com>
2020-07-16 00:35:06 +0930

    Add Models

---
+++ WebJobsDemo/Data/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebJobsDemo.Data
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ WebJobsDemo/Data/WebJobsDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebJobsDemo.Data
+{
+    public class WebJobsDbContext : DbContext
+    {
+        public WebJobsDbContext(DbContextOptions<WebJobsDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
